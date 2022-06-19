5e7f9db1165f50f05894360efce767a74b85079a
diluuasha <diluuasha22@gmail.com>
2022-06-19 16:35:02 +0500

    16

---
+++ 16/16-01.py
@@ -0,0 +1,13 @@
+pi = 3,14159
+
+
+def circle_length(radius):
+    return 2 * pi * radius
+
+
+def (radius):
+    return pi * radius ** 2
+
+
+def main(radius):
+    return 2 * pi * radius, pi * radius ** 2
